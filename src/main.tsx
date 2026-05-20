7e873f8ebc0f27152ae2e786c4ad4a744d68669b
wangjiachuan93919-sketch <wangjiachuan93919@gmail.com>
2026-05-20 16:09:01 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+import window from 'global';
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+// 🚀 终极绝招：如果浏览器在任何地方找不到 React，直接在全局窗口上硬塞一个给他！
+if (typeof window !== "undefined") {
+  (window as any).React = React;
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
